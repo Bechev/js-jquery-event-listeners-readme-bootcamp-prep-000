a75157707efac68a97b940b6a3f7c61168a56241
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-25 12:17:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+
+
+function pressIt(){
+  $('input').on('keydown',function{
+    if(key.which===103){
+      alert('You pressed G');
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  pressIt();
 
 });
