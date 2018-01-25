78d7366b55f3a12eed0013fba496a4aa36697c15
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-25 12:19:43 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
 function getIt(){
   $('p').on('click',function(){
-    alert('Hey!')
+    alert('Hey!');
+  })
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
   })
 }
 
